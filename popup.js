44f43cd797386675ec20ef0faf4c6d328e1ccf58
Kevin Ye <Kevin@LeProdeFeuille.fios-router.home>
2016-03-19 21:38:01 -0700

    Updated, works on page reload

--- popup.js
+++ popup.js
@@ -1,24 +1,33 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    message.innerText = request.source;
-    // console.log(message.innerText);
-  }
-});
-
 function onWindowLoad() {
 
-  var message = document.querySelector('#message');
-  
-    chrome.tabs.executeScript(null, {
-      file: "getPagesSource.js"
-    }, function() {
-      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-      if (chrome.runtime.lastError) {
-        message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-      }
-    });
-  
+  function DOMtoString(document_root) {
+      // Original webmail title
+      var def_title = "Caltech Webmail";
 
+      // Get inner html of left sidebar...luckily we can do this because it's
+      // given a name.
+      var html = window.frames["left"].document.documentElement.innerHTML;
+
+      // Strip some HTML
+      html = html.replace(/<(?:.|\n)*?>/gm, '');
+
+      // Parse the result of this html. The final variable either contains
+      // (number) or some character that's the result of the computation.
+      var folder_index = html.search("INBOX") + "INBOX".length + "&nbsp;".length;
+      html = html.substring(folder_index);
+      var space_index = html.search("&nbsp;");
+      html = html.substring(0, space_index);
+
+      // Apparently this is what I'm left with if there's no mail.
+      if (html.charAt(0) !== "(") {
+          document.title = html + " " + def_title;
+      }
+
+      else {
+        document.title = def_title;
+      }
+    
+    return html;
 }
 
 window.onload = onWindowLoad;
